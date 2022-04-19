f6d51b49ee4ec7b6377f6b82d824395258fb04f4
orhan <orhankagan33@gmail.com>
2022-04-19 03:12:25 +0300

    added counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.scss';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+  const[count, setCount] = useState(0);
+  function minus(){
+    setCount(prev => prev-1)
+  }
+  function plus(){
+    setCount(prev => prev+1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className={count > 0 ? "positive": count < 0 ? "negative" :null}>{count}</h1>
+      <div className="button-wrapper">
+        <button onClick={minus}>-</button>
+        <button onClick={plus}>+</button>
+      </div>
     </div>
   );
 }
-
-export default App;
